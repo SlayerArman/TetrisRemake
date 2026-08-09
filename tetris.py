32fe961cd07201a2a3c2a9746deb6f826a627b09
SlayerArman <cartoontv123360@gmail.com>
2026-08-09 18:14:41 +0500

    added down key control

--- tetris.py
+++ tetris.py
@@ -8,6 +8,7 @@ class Tetris:
         self.sprite_group = pg.sprite.Group()
         self.field_array = self.get_field_array()
         self.system = System(self)
+        self.speed_up = False
 
     def check_full_lines(self):
         row = FIELD_H - 1
@@ -34,6 +35,7 @@ class Tetris:
 
     def check_system_landing(self):
         if self.system.landing:
+            self.speed_up = False
             self.put_system_blocks_in_array()
             self.system = System(self)
 
@@ -44,6 +46,8 @@ class Tetris:
             self.system.move(direction='right')
         elif (pressed_key == pg.K_UP or pressed_key == pg.K_w):
             self.system.rotate()
+        elif (pressed_key == pg.K_DOWN or pressed_key == pg.K_s):
+            self.speed_up = True
 
     def draw_grid(self):
         for x in range(FIELD_W):
@@ -52,7 +56,8 @@ class Tetris:
                              (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE), 1)
 
     def update(self):
-        if self.app.anim_trigger:
+        trigger = [self.app.anim_trigger, self.app.fast_anim_trigger][self.speed_up]
+        if trigger:
             self.system.update()
             self.check_system_landing()
             self.check_full_lines()
